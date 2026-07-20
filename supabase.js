9852385f181ba115b39a1d62304279876448d663
Rajveerrr001 <rajveerbhambhii@gmail.com>
2026-07-20 18:50:41 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "YOUR_PROJECT_URL" NEXT_PUBLIC_SUPABASE_URL=https://yjpfijmhrowaqynavyox.supabase.co
+const SUPABASE_KEY = "YOUR_ANON_KEY"; NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_WgVZaVJM9izli03YIDlVXQ_ku9Uw1eO
 
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
